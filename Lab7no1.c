1af7de1603e0e548fbe8039679ba766c45f77e1e
s6704062610011 <s6704062610011@email.kmutnb.ac.th>
2025-01-14 16:20:12 +0700

    Add files via upload

--- Lab7no1.c
+++ Lab7no1.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 double functionM(void);
 int functionN(int n, double x);
 double functionO(double, int, double, int);
@@ -8,15 +7,15 @@ void main(void) {
   	int a, b, c, d, e;
   	double r, s, t, u, v;
 }
-//a)  functionM();
-//c)  b = functionN(a, b);
-//d)  r = functionO(r,a,s,b);
-//e)  s = functionP(a,b,c,d,e);
-//f)  u = functionM();
-//g)  c = d + functionN(r,s);
-//h)  t = s * functionO(r, a, r, a);
-//i)  a = v + functionP(r, s, t, t);
-//j)  functionP(functionN(a, a), s, t, t+r);
-//k)  
-v = functionP(functionN(a, a), s, t, t+r);
-
+//a)  functionM(); correct
+//b)  a = functionM(); incorrect
+//c)  b = functionN(a, b); correct
+//d)  r = functionO(r,a,s,b); incorrect
+//e)  s = functionP(a,b,c,d,e); incorrect
+//f)  u = functionM(); correct
+//g)  c = d + functionN(r,s); incorrect
+//h)  t = s * functionO(r, a, r, a); correct
+//i)  a = v + functionP(r, s, t, t); incorrect
+//j)  functionP(functionN(a, a), s, t, t+r); incorrect
+//k)  v = functionP(functionN(a, a), s, t, t+r); incorrect
+ 
